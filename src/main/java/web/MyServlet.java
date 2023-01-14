87cf46a55ace31db6faff63c6f6fd5831ecb7ff9
chebihiF <chebihi.f@ucd.ac.ma>
2023-01-14 17:20:14 +0100

    add servlet mapping

--- src/main/java/web/MyServlet.java
+++ src/main/java/web/MyServlet.java
@@ -1,11 +1,13 @@
 package web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = {"/coco","/toto"})
 public class MyServlet extends HttpServlet {
 
     @Override
